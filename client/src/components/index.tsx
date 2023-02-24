99088bcbf9c243cece92453ce9942cd1b28f0911
Sahil Halbe <sahilyhalbe@gmail.com>
2023-02-24 10:32:03 +0530

    first commit

---
+++ client/src/components/index.tsx
@@ -0,0 +1,17 @@
+// common
+import Profile from "./common/Profile";
+import PropertyCard from "./common/PropertyCard";
+import CustomButton from "./common/CustomButton";
+
+// charts
+import PieChart from "./charts/PieChart";
+import PropertyReferrals from "./charts/propertyReferrals";
+import TotalRevenue from "./charts/totalRevenue";
+
+// agent
+import AgentCard from "./agent/agentCard";
+
+// home
+import TopAgent from "./home/TopAgent";
+
+export { Profile, PropertyCard, CustomButton, PieChart, PropertyReferrals, TotalRevenue, AgentCard, TopAgent };
