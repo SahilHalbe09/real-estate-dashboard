501ccc875a9d4b942299f6d8e287a98bc95aea8e
Sahil Halbe <sahilyhalbe@gmail.com>
2023-02-24 11:20:44 +0530

    second commit - changed favicon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -34,7 +34,7 @@ function App() {
 			const profileObj = credential ? parseJwt(credential) : null;
 
 			if (profileObj) {
-				const response = await fetch("http://localhost:8080/api/v1/users", {
+				const response = await fetch("https://real-estate-dashboard-ggei.onrender.com/api/v1/users", {
 					method: "POST",
 					headers: { "Content-Type": "application/json" },
 					body: JSON.stringify({
@@ -103,7 +103,7 @@ function App() {
 			<GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
 			<RefineSnackbarProvider>
 				<Refine
-					dataProvider={dataProvider("http://localhost:8080/api/v1")}
+					dataProvider={dataProvider("https://real-estate-dashboard-ggei.onrender.com/api/v1")}
 					notificationProvider={notificationProvider}
 					ReadyPage={ReadyPage}
 					catchAll={<ErrorComponent />}
